afc8d3aff0bc094645f539490ba1ad0016a03af0
Nico-Mayer <nico.m94@icloud.com>
2023-12-20 13:46:52 +0100

    Update page title and meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,15 +3,25 @@
 </script>
 
 <svelte:head>
-	<title>LOFI | FLOW</title>
+	<title>lofi flow.</title>
 
-	<meta property="og:title" content="lofi flow" />
+	<meta
+		name="description"
+		content="Save your best-loved YouTube lofi live radios and videos in one spot." />
+	<meta property="og:title" content="lofi flow." />
 	<meta
 		property="og:description"
 		content="Save your best-loved YouTube lofi live radios and videos in one spot." />
-	<meta property="og:image" content="/preview.png" />
 	<meta property="og:url" content="https://lofi-flow.vercel.app/" />
 	<meta property="og:type" content="website" />
+	<meta property="og:image" content="/preview.png" />
+
+	<meta name="twitter:title" content="lofi flow." />
+	<meta
+		name="twitter:description"
+		content="Save your best-loved YouTube lofi live radios and videos in one spot." />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:image" content="/preview.png" />
 </svelte:head>
 
 <slot />
