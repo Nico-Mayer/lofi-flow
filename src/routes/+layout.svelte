a87f5265da8d17a6239ff08f6b5b009e8fac5df9
Nico-Mayer <nico.m94@icloud.com>
2023-12-20 13:41:50 +0100

    Remove og:image:width and og:image:height meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,8 +10,6 @@
 		property="og:description"
 		content="Save your best-loved YouTube lofi live radios and videos in one spot." />
 	<meta property="og:image" content="/preview.png" />
-	<meta property="og:image:width" content="1200" />
-	<meta property="og:image:height" content="630" />
 	<meta property="og:url" content="https://lofi-flow.vercel.app/" />
 	<meta property="og:type" content="website" />
 </svelte:head>
