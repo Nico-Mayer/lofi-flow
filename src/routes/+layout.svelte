8bd0dc35ddf8280ebe94bbf68318cd0f78cbaf19
Nico-Mayer <nico.m94@icloud.com>
2023-12-20 13:20:29 +0100

    Update OG meta tags in app.html and +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,14 @@
 
 <svelte:head>
 	<title>LOFI | FLOW</title>
+
+	<meta property="og:title" content="lofi flow" />
+	<meta
+		property="og:description"
+		content="Save your best-loved YouTube lofi live radios and videos in one spot." />
+	<meta property="og:image" content="/preview.png" />
+	<meta property="og:url" content="https://lofi-flow.vercel.app/" />
+	<meta property="og:type" content="website" />
 </svelte:head>
 
 <slot />
