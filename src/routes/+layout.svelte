6a91efaef491c09bd1e9339723030215213d88ac
mtdev94 <tsaassou@gmail.com>
2024-01-14 05:12:42 +0100

    Added vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
     import "../app.css";
+    import { dev } from "$app/environment";
+    import { inject } from "@vercel/analytics";
+
+    inject({ mode: dev ? "development" : "production" });
 </script>
 
 <svelte:head>
